687e7d990f6a2b28d2d052398b5a4191bdb24364
Anton <anton.dorosev@yandex.ru>
2025-10-22 16:55:01 +0300

    add authentication

---
+++ app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from app.schemas.user import UserCreate, UserLogin, UserResponse
+from app.schemas.auth import AuthResponse
+
+__all__ = ["UserCreate", "UserLogin", "UserResponse", "AuthResponse"]
---
+++ app/schemas/auth.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class AuthResponse(BaseModel):
+    user_id: int
+    full_name: str
+    email: str
+    role: str
+    token: str
+
+    class Config:
+        from_attributes = True
